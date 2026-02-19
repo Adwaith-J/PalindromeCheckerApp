ef926423e48c9416f09cdbfa3a930b163a5f3625
Adwaith-J <nirbhayrai712@gmail.com>
2026-02-19 08:50:08 +0530

    UC1 solved

---
+++ PalindromeCheckerApp/src/PalindromeCheckerApp.java
@@ -0,0 +1,13 @@
+/*UC1
+ * @author developer
+ * @version 1.0
+ */
+
+public class PalindromeCheckerApp {
+
+    public static void main(String[] args){
+        System.out.println("Welcome to Palindrome Checker Management System");
+        System.out.println("Version: 1.0");
+        System.out.println("System Initialized Successfully");
+    }
+}
